7487d1864f5d43af418a380b896f50533be93390
Mariosts95 <marios.tsioutsis1@gmail.com>
2022-02-28 22:51:49 +0200

    Add MUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+
+// @Material UI
+import Button from '@mui/material/Button';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,33 +13,14 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello Vite + React!</p>
+        <p>Hello Vite + React + MUI!</p>
         <p>
-          <button type='button' onClick={() => setCount((count) => count + 1)}>
+          <Button
+            variant='contained'
+            onClick={() => setCount((count) => count + 1)}
+          >
             count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className='App-link'
-            href='https://vitejs.dev/guide/features.html'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Vite Docs
-          </a>
+          </Button>
         </p>
       </header>
     </div>
