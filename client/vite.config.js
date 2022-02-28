da400880ce9ead5a44120603097557a46fd7caa4
Mariosts95 <marios.tsioutsis1@gmail.com>
2022-02-28 23:20:32 +0200

    Initialize DelSuites styles and basic assets

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import reactScopedCssPlugin from 'rollup-plugin-react-scoped-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), reactScopedCssPlugin()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/sass/variables";',
+      },
+    },
+  },
+});
