55ed0be3f873a68f6766cdeba968df54dfb45907
Mariosts95 <marios.tsioutsis1@gmail.com>
2022-03-13 22:13:18 +0200

    Update Hotel Card components

--- client/src/components/UI/Card.jsx
+++ client/src/components/UI/Card.jsx
@@ -1,100 +1,47 @@
-import { useState } from 'react';
-import { styled } from '@mui/material/styles';
+// @Material UI
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 import StarsRating from './StarsRating';
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
-export default function RecipeReviewCard() {
-  const [expanded, setExpanded] = useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
+const NewCard = () => {
   return (
     <Card sx={{ backgroundColor: 'secondary.light' }}>
-      <CardHeader title='Hotel Name' subheader='Street Number, City, Country' />
       <CardMedia
-        component='img'
+        sx={{ pt: '56.25%' }}
         image='https://source.unsplash.com/random'
-        alt='Random image'
-        sx={{ maxHeight: 400 }}
+        alt='hotel img'
       />
       <CardContent>
+        <Typography gutterBottom variant='h5' component='h5'>
+          Hotel Name
+        </Typography>
+        <Typography gutterBottom variant='body2'>
+          City, Country
+        </Typography>
+
         <StarsRating />
+
         <Typography variant='body2' color='text.secondary'>
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          Hotel description hotel description hotel description hotel
+          description hotel description hotel description hotel description
+          hotel description hotel description hotel description
         </Typography>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label='add to favorites'>
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label='share'>
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label='show more'
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
+      <CardActions>
+        <Button size='small' variant='contained'>
+          Learn More
+        </Button>
+        <Button size='small' variant='outlined'>
+          View on Google Maps
+        </Button>
       </CardActions>
-      <Collapse in={expanded} timeout='auto' unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
     </Card>
   );
-}
+};
+
+export default NewCard;
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,15 +1,15 @@
 // @Material UI
 import Grid from '@mui/material/Grid';
-import RecipeReviewCard from '../components/UI/Card';
+import Card from '../components/UI/Card';
 
-const dummy = [...Array(25)];
+const dummy = [...Array(9)];
 
 const Home = () => {
   return (
     <Grid container spacing={3} sx={{ pl: 3, pr: 3 }}>
       {dummy.map((card, index) => (
         <Grid key={index} item xs={12} sm={6} lg={4} xl={3}>
-          <RecipeReviewCard />
+          <Card />
         </Grid>
       ))}
     </Grid>
