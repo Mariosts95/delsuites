693185dce11c1ef2056353cd538172ee65053494
Mariosts95 <marios.tsioutsis1@gmail.com>
2022-05-17 21:08:58 +0300

    Remove enviromental variables from client

--- client/src/services/hotels.js
+++ client/src/services/hotels.js
@@ -2,21 +2,21 @@ import axios from 'axios';
 
 // fetch hotels from API
 const fetchHotels = async (from = 0, size = 10) => {
-  return await axios.get(`${import.meta.env.VITE_API_BASE_PATH}/hotels`, {
+  return await axios.get(`/hotels`, {
     params: { from: from - 1, size },
   });
 };
 
 // fetch hotels pages from API
 const fetchHotelsPages = async (size = 10) => {
-  return await axios.get(`${import.meta.env.VITE_API_BASE_PATH}/hotels/info`, {
+  return await axios.get(`/hotels/info`, {
     params: { size },
   });
 };
 
 // fetch specific hotel from API
 const fetchHotel = async (id) => {
-  return await axios.get(`${import.meta.env.VITE_API_BASE_PATH}/hotel/${id}`);
+  return await axios.get(`/hotel/${id}`);
 };
 
 export { fetchHotels, fetchHotel, fetchHotelsPages };
